3ed7654480697b46b92b02932375e03a3af67f8b
Shad Kaske <shadkaske@gmail.com>
2020-06-21 17:05:48 -0500

    Set up script

---
+++ .local/bin/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# System update
+sudo apt update && sudo apt dist-upgrade
+
+sudo apt install neovim tmux tmuxinator zsh build-essential xclip git-flow \
+    curl python3-pip python-pip ruby-dev tmuxinator silversearcher-ag \
+    exuberant-ctags nodejs npm breeze-cursor-theme unzip papirus-icon-theme
+
+fc-cach -fv
+
+
+
+# Install Kitty Terminal

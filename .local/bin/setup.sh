c0654798c5c81a2a5494c5a7ee234ef4886161f8
Shad Kaske <shadkaske@gmail.com>
2020-06-21 18:28:54 -0500

    Kitty Install

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Grab git submodules
+alias dots='git --work-tree=$HOME --git-dir=$HOME/.dots'
+
+dots submodule update --init
+
 # System update
 sudo apt update && sudo apt dist-upgrade -y
 
@@ -10,6 +15,9 @@ sudo apt install -y neovim tmux tmuxinator zsh build-essential xclip git-flow \
 
 fc-cache -fv
 
-
-
 # Install Kitty Terminal
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+sudo update-alternatives --install /usr/bin/x-terminal-emulator \
+    x-terminal-emulator ~/.local/kitty.app/bin/kitty 50
+sudo update-alternatives --set x-terminal-emulator ~/.local/kitty.app/bin/kitty
+cp ${HOME}/.config/kitty/kitty.desktop ~/.local/share/applications

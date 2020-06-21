d8dfb8feafb4dd53fc4109f4c962657e17b8662a
Shad Kaske <shadkaske@gmail.com>
2020-06-21 18:42:54 -0500

    neovim setup

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Grab git submodules
-alias dots='git --work-tree=$HOME --git-dir=$HOME/.dots'
-
-dots submodule update --init
+git --work-tree=$HOME --git-dir=$HOME/.dots submodule update --init
 
 # System update
 sudo apt update && sudo apt dist-upgrade -y
@@ -11,13 +9,20 @@ sudo apt update && sudo apt dist-upgrade -y
 sudo apt install -y neovim tmux tmuxinator zsh build-essential xclip git-flow \
     curl python3-pip python2 ruby-dev tmuxinator silversearcher-ag \
     exuberant-ctags nodejs npm breeze-cursor-theme unzip papirus-icon-theme \
+    kitty
 
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+
+sudo python2 get-pip.py
+
+rm -rf get-pip.py
+
+sudo gem install neovim
+
+sudo npm -g install neovim
+
+python3 -m pip install neovim
+
+python2 -m pip install neovim
 
 fc-cache -fv
-
-# Install Kitty Terminal
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-sudo update-alternatives --install /usr/bin/x-terminal-emulator \
-    x-terminal-emulator ~/.local/kitty.app/bin/kitty 50
-sudo update-alternatives --set x-terminal-emulator ~/.local/kitty.app/bin/kitty
-cp ${HOME}/.config/kitty/kitty.desktop ~/.local/share/applications

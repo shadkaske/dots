b93b6cd1bb8b278b308c7d4f00102b7bf399c147
Shad Kaske <shadkaske@gmail.com>
2020-06-21 18:16:34 -0500

    Working on setup script

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # System update
-sudo apt update && sudo apt dist-upgrade
+sudo apt update && sudo apt dist-upgrade -y
 
-sudo apt install neovim tmux tmuxinator zsh build-essential xclip git-flow \
-    curl python3-pip python-pip ruby-dev tmuxinator silversearcher-ag \
-    exuberant-ctags nodejs npm breeze-cursor-theme unzip papirus-icon-theme
+sudo apt install -y neovim tmux tmuxinator zsh build-essential xclip git-flow \
+    curl python3-pip python2 ruby-dev tmuxinator silversearcher-ag \
+    exuberant-ctags nodejs npm breeze-cursor-theme unzip papirus-icon-theme \
 
-fc-cach -fv
+
+fc-cache -fv
 
 
 

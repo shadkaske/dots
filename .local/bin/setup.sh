55322008b9642164e80d1aaa4e19fb140c13ef7f
Shad Kaske <shadkaske@gmail.com>
2020-06-23 07:38:59 -0500

    Removed submodules added git clones to setup

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -1,32 +1,57 @@
 #!/bin/bash
 
-# Grab git submodules
-git --work-tree=$HOME --git-dir=$HOME/.dots submodule update --init
+# create direcotries if needed
+if [[ ! -d "$HOME/.local/share" ]]; then
+    mkdir -p $HOME/.local/share
+fi
+
+if [[ ! -d "$HOME/.local/share/zsh-customs" ]]; then
+    mkdir -p $HOME/.local/share/zsh-customs
+fi
+
+# Clone oh-my-zsh
+if [[ ! -d "$HOME/.local/share/oh-my-zsh" ]]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git \
+    $HOME/.local/share/oh-my-zsh
+fi
+
+# Install zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    $HOME/.local/share/zsh-customs/plugins/zsh-syntax-highlighting
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    $HOME/.local/share/zsh-customs/themes/powerlevel10k
+
+# Clone base16 repos for gnome terminal and shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# Spacemacs
+git clone --single-branch --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # System update
 sudo apt update && sudo apt dist-upgrade -y
 
 sudo apt install -y neovim tmux tmuxinator zsh build-essential xclip git-flow \
-    curl python3-pip python2 ruby-dev tmuxinator silversearcher-ag \
-    exuberant-ctags nodejs npm breeze-cursor-theme unzip kitty \
-    curl dconf-editor software-properties-common apt-transport-https wget \
-    nautilus-dropbox emacs evolution evolution-ews openjdk-11-jdk virtualbox \
-    virtualbox-dkms virtualbox-ext-pack virtualbox-guest-additions-iso \
-    remmina remmina-plugin-rdp wine winetricks gnome-tweaks
+    silversearcher-ag breeze-cursor-theme unzip kitty curl \
+    software-properties-common apt-transport-https wget nautilus-dropbox emacs \
+    evolution evolution-ews openjdk-11-jdk virtualbox virtualbox-dkms \
+    virtualbox-ext-pack virtualbox-guest-additions-iso remmina \
+    remmina-plugin-rdp wine winetricks gnome-tweaks
 
-curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+#curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
 
-sudo python2 get-pip.py
+#sudo python2 get-pip.py
 
-rm -rf get-pip.py
+#rm -rf get-pip.py
 
-sudo gem install neovim
+#sudo gem install neovim
 
-sudo npm -g install neovim
+#sudo npm -g install neovim
 
-python3 -m pip install neovim
+#python3 -m pip install neovim
 
-python2 -m pip install neovim
+#python2 -m pip install neovim
 
 #VSCode
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -

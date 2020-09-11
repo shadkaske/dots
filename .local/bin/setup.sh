ab2225a2d8ab7e21379468eab4ca3c181295595b
Shad Kaske <shadkaske@gmail.com>
2020-09-11 12:24:07 -0500

    Setup, greenclip config and backintime updates

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -22,6 +22,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
 # Clone base16 repos for gnome terminal and shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+# Install FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+$HOME/.fzf/install --no-fish --all
+
 # System update
 sudo apt update && sudo apt dist-upgrade -y
 
@@ -29,9 +33,9 @@ sudo apt update && sudo apt dist-upgrade -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 # Add Yarn PPA
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | \
-    sudo tee /etc/apt/sources.list.d/yarn.list
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | \
+#     sudo tee /etc/apt/sources.list.d/yarn.list
 
 # add vscode ppa
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
@@ -42,7 +46,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 # Add PHP PPA
-sudo apt-add-repository --yes --no-update ppa:ondrej/php
+# sudo apt-add-repository --yes --no-update ppa:ondrej/php
 
 sudo apt update
 
@@ -51,43 +55,43 @@ sudo apt install --yes neovim tmux tmuxinator zsh build-essential \
     software-properties-common apt-transport-https nautilus-dropbox \
     evolution evolution-ews default-jre default-jdk remmina remmina-plugin-rdp  \
     gnome-tweaks ca-certificates gnupg-agent autokey-gtk nmap \
-    nodejs mysql-server php7.2-bcmath php7.2-bz2 php7.2-cli \
-    php7.2-common php7.2-curl php7.2-fpm php7.2-gd php7.2-imap php7.2-intl \
-    php7.2-json php7.2-ldap php7.2-mbstring php7.2-mysql php7.2-odbc \
-    php7.2-opcache php7.2-readline php7.2-xml python3 python2 ruby-full \
-    python3-pip libnss3-tools jq xsel nginx papirus-icon-theme \
-    qt5-style-kvantum qt5-style-kvantum-themes \
-    gconf2 gconf-service libappindicator1 network-manager-l2tp-gnome \
+    nodejs python3 python2 ruby-full python3-pip libnss3-tools jq xsel
+    papirus-icon-theme qt5-style-kvantum qt5-style-kvantum-themes gconf2 \
+    gconf-service libappindicator1 network-manager-l2tp-gnome \
     network-manager-strongswan libstrongswan-standard-plugins \
     libstrongswan-extra-plugins onedrive wine winetricks timeshift \
-    backintime-qt4 dnsmasq inotify-tools code google-chrome-stable \
-    libavcodec-extra58 xournal htop nautilus-admin qemu-kvm libvirt-clients \
-    libvirt-daemon-system bridge-utils virt-manager libvirt-daemon ovmf \
-    spice-vdagent virt-viewer qemu mlocate
+    backintime-qt4 inotify-tools code google-chrome-stable \
+    libavcodec-extra58 xournal htop nautilus-admin mlocate feh arandr \
+    compton unclutter sxhkd qutebrowser ranger virtualbox virtualbox-dkms \
+    virtualbox-guest-additions-iso virtualbox-ext-pack libx11-dev libxft-dev \
+    libxinerama-dev libharfbuzz-dev lastpass-cli dunst
 
 # Remove Apps we don't want
 sudo apt remove --yes --purge geary
 
 sudo apt autoremove --yes
 
+# Wget Greeclip Binary
+wget -o $HOME/.local/bin/greenclip https://github.com/erebe/greenclip/releases/download/3.3/greenclip
+
 # install yarn, just skip the recomended packages
-sudo apt install --no-install-recommends yarn
+# sudo apt install --no-install-recommends yarn
 
 # Install Composer
-curl -sS https://getcomposer.org/installer -o composer-setup.php
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+# curl -sS https://getcomposer.org/installer -o composer-setup.php
+# sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-rm -f composer-setup.php
+# rm -f composer-setup.php
 
-# install valet
-composer global require cpriego/valet-linux
-$HOME/.config/composer/vendor/bin/valet install
+# # install valet
+# composer global require cpriego/valet-linux
+# $HOME/.config/composer/vendor/bin/valet install
 
-curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+# curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
 
-sudo python2 get-pip.py
+# sudo python2 get-pip.py
 
-rm -rf get-pip.py
+# rm -rf get-pip.py
 
 sudo gem install neovim
 
@@ -95,7 +99,7 @@ sudo npm -g install neovim
 
 python3 -m pip install neovim neovim-remote
 
-python2 -m pip install neovim
+# python2 -m pip install neovim
 
 nvim -u $HOME/.config/nvim/plugins.vim +PlugInstall +CocInstall +qall
 
@@ -140,25 +144,29 @@ fc-cache -fv
 sudo update-alternatives --set x-terminal-emulator /usr/bin/kitty
 
 # Add libvirt group
-sudo adduser $USER libvirt
-sudo adduser $USER libvirt-qemu
-sudo adduser $USER kvm
+# sudo adduser $USER libvirt
+# sudo adduser $USER libvirt-qemu
+# sudo adduser $USER kvm
+sudo useradd $USER vboxusers
 
 # change compression to no for vm dir
-mkdir -p $HOME/.local/share/libvirt
-sudo chattr +C ~/.local/share/libvirt
-sudo chattr +C /var/run/libvirt
+# mkdir -p $HOME/.local/share/libvirt
+# sudo chattr +C ~/.local/share/libvirt
+# sudo chattr +C /var/run/libvirt
+mkdir $HOME/VirtualBox\ VMs
+sudo chattr +C $HOME/VirtualBox\ VMs
 
 # Copy samba config and enable
-sudo cp $HOME/.config/system/smb.conf /etc/samba/smb.conf
-sudo systemctl enable smbd
-sudo systemctl start smbd
+# sudo cp $HOME/.config/system/smb.conf /etc/samba/smb.conf
+# sudo systemctl enable smbd
+# sudo systemctl start smbd
 
 # enable libvirt daemon
-sudo systemctl enable libvirtd
+# sudo systemctl enable libvirtd
 
 # Set kvantum environment variable
 echo "export QT_STYLE_OVERRIDE=kvantum" >> ~/.profile
+echo "export LPASS_CLIPBOARD_COMMAND="xclip -selection clipboard -in -l 1"" >> ~/.profile
 
 # Import dconf settings
 dconf load / < $HOME/.local/share/dconf-user-settings.conf

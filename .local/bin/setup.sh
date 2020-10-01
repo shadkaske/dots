47093a35f06055bfe1b9ec882a3a048099e42bce
Shad Kaske <shadkaske@gmail.com>
2020-10-01 16:45:24 -0500

    Added rofi install

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -74,7 +74,7 @@ sudo apt install --yes neovim tmux tmuxinator zsh build-essential xclip git-flow
     php7.2-xdebug php7.2-bcmath php7.2-bz2 php7.2-cli php7.2-common php7.2-curl php7.2-fpm \
     php7.2-gd php7.2-imap php7.2-intl php7.2-json php7.2-ldap php7.2-mbstring php7.2-mysql \
     php7.2-odbc php7.2-opcache php7.2-readline php7.2-sqlite3 php7.2-xml mysql-server virt-manager \
-    qemu libvirt-daemon ovmf virt-viewer libvirt-clients samba snapd
+    qemu libvirt-daemon ovmf virt-viewer libvirt-clients samba snapd rofi
 
 # Remove Apps we don't want
 sudo apt remove --yes --purge geary

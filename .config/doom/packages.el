b60d62ec7f79501b9b6265cc6c3a637fa9078033
Shad Kaske <shadkaske@gmail.com>
2021-02-12 11:39:33 -0600

    Added org-gtd to Emacs packages

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -9,7 +9,7 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 ;; (package! org)
-;; (package! org-gtd)
+(package! org-gtd)
 ;; (package! org-super-agenda)
 ;; (package! org-bullets)
 (package! all-the-icons-dired)

3cb62e9c0d4f4738f1e33091c9753dbb0ac4f394
Shad Kaske <shadkaske@gmail.com>
2021-02-12 11:39:54 -0600

    Turned off literate mode for emacs config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,16 +1,23 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Basic Emacs Setup
 (setq user-full-name "Shad Kaske"
     user-mail-address "shadkaske@gmail.com")
 
+;; Don't Ask to Close
 (setq confirm-kill-emacs nil)
 
+;; Disable Clipboard Selection and Manager
 (setq select-enable-clipboard nil
     x-select-enable-clipboard-manager nil)
 
+;; Theme Settings
 (setq doom-theme 'doom-material)
 (after! doom-themes
 (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
 
+;; Font Settings
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16)
     doom-variable-pitch-font (font-spec :family "Ubuntu" :size 16)
     doom-big-font (font-spec :family "JetBrainsMono Nerd Font" :size 24))
@@ -18,20 +25,27 @@
 '(font-lock-comment-face :slant italic)
 '(font-lock-keyword-face :slant italic))
 
+;; Relatvie Line Numbers
 (setq display-line-numbers-type 'relative)
 
+;; Line Height / Spacing
 ;; (setq default-text-properties '(line-spacing 0.1  line-height 1.1))
 
+;; All The Icons Scale
 (after! all-the-icons
     (setq all-the-icons-scale-factor 1.0))
 
+;; Show Trailing Whitespace
 (setq show-trailing-whitespace t)
 
+;; Custom Keybindings
+;;;; Evil Mode Line Inserts
 (map!
  (:after evil
     :m "] SPC" #'evil-motion-insert-newline-below
     :m "[ SPC" #'evil-motion-insert-newline-after))
 
+;;;; Evil Mode Simplified Navigation
 (map!
  (:after evil
    :en "C-h"   #'evil-window-left
@@ -39,6 +53,7 @@
    :en "C-k"   #'evil-window-up
    :en "C-l"   #'evil-window-right))
 
+;; mu4e
 (remove-hook! 'mu4e-compose-pre-hook #'org-msg-mode)
 (after! mu4e
 (setq! mu4e-maildir (expand-file-name "~/Mail/kaskshad-showplace") ; the rest of the mu4e folders are RELATIVE to this one
@@ -84,18 +99,25 @@
 (mu4e-alert-set-default-style 'libnotify)
 (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
 
+;; Org Mode
+;;;; Doom Emacs Org Settings
 (setq org-directory "~/Nextcloud/org/")
 (setq org-noter-notes-search-path '("~/Nextcloud/org/reference/"))
 
+;;;; Hide Emphasis Markers
 (after! org (setq org-hide-emphasis-markers t)
+  ;;;; Turn On Logging
   (setq org-log-done t)
   (setq org-log-into-drawer t))
 
+;;;; Disable Electric Indent Mode
 (add-hook! org-mode (electric-indent-local-mode -1))
 
+;;;; Use Variable Pitch Fonts in Org Mode
 (add-hook! org-mode :append
            #'variable-pitch-mode)
 
+;;;; Org Agenda
 (use-package! org-agenda
   :ensure t
   :after org
@@ -103,14 +125,16 @@
   (org-agenda-files (list org-directory))
   (setq org-agenda-custom-commands '(("g" "Scheduled today and all NEXT items" ((agenda "" ((org-agenda-span 1))) (todo "NEXT"))))))
 
+;;;; Archiving
 (after! org
   (setq org-archive-tag "archive"))
   (setq org-archive-location (concat org-directory "archive.org::* From %s"))
 
+;;;; Refile Targets
 (after! org
   (setq org-refile-targets '((nil :maxlevel . 3)
                              (org-agenda-files :maxlevel . 3))))
-
+;; NeoTree
 (after! neotree
     (setq neo-smart-open t
           neo-windows-fixed-size nil))

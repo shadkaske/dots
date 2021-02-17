901834190bebcb9980f42aed4779bc94ae3a67d6
Shad Kaske <shadkaske@gmail.com>
2021-02-17 17:29:02 -0600

    Updated Init Script

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -39,3 +39,33 @@ sudo add-apt-repository ppa:kelleyk/emacs --no-update --yes
 sudo sed -i 's/groovy/focal/g' /etc/apt/sources.list.d/kelleyk-ubuntu-emacs-groovy.list
 
 sudo apt update
+
+sudo apt install emacs27 --yes
+
+# Install Lazy Git
+sudo add-apt-repository ppa:lazygit-team/release --yes
+sudo apt install lazygit --yes
+
+# Make sure oh-my-zsh is installed
+[ ! -d $HOME/.oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git \
+    $HOME/.oh-my-zsh
+
+# Make sure fzf is installed
+[ ! -d $HOME/.fzf ] && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
+    $HOME/.fzf/install --no-fish --all
+
+# Check if the PowerLevel10k Plugin is install
+[ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+# Check if the Autosuggestions Plugin is installed
+[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] && git clone https://github.com/zsh-users/zsh-autosuggestions \
+    $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+# Check if the Syntax Highlight Plugin is installed
+[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Check if Base16-Shell is Installed
+[ ! -d $HOME/.config/base16-shell ] && git clone https://github.com/chriskempson/base16-shell.git \
+    $HOME/.config/base16-shell

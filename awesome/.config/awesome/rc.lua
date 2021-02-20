c22f011fe90bb9278cd73e2e17b22cc15ad6f48a
Shad Kaske <shadkaske@gmail.com>
2021-02-20 14:47:25 -0600

    Added Xfce power manager

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -703,6 +703,7 @@ run_once({
     "xfce4-clipman",
     "unclutter -root",
     "xfce4-screensaver",
+    "xfce4-power-manager --sm-client-disable",
     -- "onedrive monitor",
     "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1",
     "udevadm monitor",

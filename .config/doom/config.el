4ef53a2f575889debe1d93b15b0b142a8b807a8f
Shad Kaske <shadkaske@gmail.com>
2021-02-25 11:31:52 -0600

    Doom updates

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -17,9 +17,21 @@
 (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
 
+;; Variable Pitch Font Setting Function
+(defun smk/set-buffer-variable-pitch ()
+  (interactive)
+  (variable-pitch-mode t)
+  (setq line-spacing 3)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-link nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-date nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-special-keyword nil :inherit 'fixed-pitch))
+
 ;; Font Settings
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16)
-    doom-variable-pitch-font (font-spec :family "Ubuntu" :size 16)
+    doom-variable-pitch-font (font-spec :family "Cantarell" :size 16)
     doom-big-font (font-spec :family "JetBrainsMono Nerd Font" :size 24))
 (custom-set-faces!
 '(font-lock-comment-face :slant italic)
@@ -29,7 +41,7 @@
 (setq display-line-numbers-type 'visual)
 
 ;; Line Height / Spacing
-;; (setq default-text-properties '(line-spacing 0.1  line-height 1.1))
+(setq default-text-properties '(line-spacing 0.1  line-height 1.1))
 
 ;; All The Icons Scale
 (after! all-the-icons
@@ -45,60 +57,71 @@
     :m "] SPC" #'evil-motion-insert-newline-below
     :m "[ SPC" #'evil-motion-insert-newline-after))
 
-;;;; Evil Mode Simplified Navigation
-;; (map!
-;;  (:after evil
-;;    :en "C-h"   #'evil-window-left
-;;    :en "C-j"   #'evil-window-down
-;;    :en "C-k"   #'evil-window-up
-;;    :en "C-l"   #'evil-window-right))
-
 ;; mu4e
-(remove-hook! 'mu4e-compose-pre-hook #'org-msg-mode)
+;;;; Set the load path for mu4e
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+
 (after! mu4e
-(setq! mu4e-maildir (expand-file-name "~/Mail/kaskshad-showplace") ; the rest of the mu4e folders are RELATIVE to this one
-        mu4e-get-mail-command "mbsync -c ~/.config/mu4e/mbsyncrc -a"
-        mu4e-index-update-in-background t
-        mu4e-compose-signature-auto-include nil
-        mu4e-use-fancy-chars t
-        mu4e-view-show-addresses t
-        mu4e-view-show-images t
-        mu4e-compose-format-flowed t
-        ;mu4e-compose-in-new-frame t
-        mu4e-change-filenames-when-moving t ;; Setting this to true causes issues tracking refiled emails that are link from org docs
-        mu4e-maildir-shortcuts
-        '( ("/Inbox" . ?i)
-            ("/Archive" . ?a)
-            ("/Drafts" . ?d)
-            ("/Deleted Items" . ?t)
-            ("/Sent Items" . ?s))
+  (require 'mu4e-org)
+  (remove-hook! 'mu4e-compose-pre-hook 'org-msg-mode)
+  (setq! mu4e-attachment-dir "~/Downloads")
+  (setq! mu4e-maildir (expand-file-name "~/Mail")
+         mu4e-get-mail-command "mbsync -a"
+         mu4e-update-interval 300
+         mu4e-index-update-in-background t
+         mu4e-compose-signature-auto-include nil
+         mu4e-view-show-addresses t
+         mu4e-view-show-images t
+         mu4e-use-fancy-chars t
+         mu4e-compose-format-flowed t
+         mu4e-change-filenames-when-moving t
+         mu4e-sent-messages-behavior 'sent)
 
-        ;; Message Formatting and sending
-        message-send-mail-function 'smtpmail-send-it
-        message-signature-file "~/.config/mu4e/mailsignature"
-        message-citation-line-format "On %a %d %b %Y at %R, %f wrote:\n"
-        message-citation-line-function 'message-insert-formatted-citation-line
-        message-kill-buffer-on-exit t))
+  (setq! mu4e-contexts
+         (list
+          ;; Work Account
+          (make-mu4e-context
+           :name "Showplace"
+           :match-func
+           (lambda (msg)
+             (when msg (string-prefix-p "/kaskshad-showplace" (mu4e-message-field msg :maildir) t)))
+           :vars '((user-mail-address . "skaske@showplacewood.com")
+                   (mu4e-drafts-folder . "/kaskshad-showplace/Drafts")
+                   (mu4e-sent-folder . "/kaskshad-showplace/Sent Items")
+                   (mu4e-refile-folder . "/kaskshad-showplace/Archive")
+                   (mu4e-trash-folder . "/kaskshad-showplace/Deleted Items")
+                   (mu4e-compose-signature . (concat
+                                              "Shad Kaske\n"
+                                              "Systems Administrator\n"
+                                              "Showplace Cabinetry\n"
+                                              "Shad.Kaske@showplacecabinetry.com\n"
+                                              "(605) 743-5006"))))
+          (make-mu4e-context
+           ;; Personal Account
+           :name "Gmail"
+           :match-func
+           (lambda (msg)
+             (when msg (string-prefix-p "/gmail" (mu4e-message-field msg :maildir) t)))
+           :vars '((user-mail-address . "shadkaske@gmail.com")
+                   (mu4e-drafts-folder . "/gmail/Drafts")
+                   (mu4e-sent-folder . "/gmail/Sent Mail")
+                   (mu4e-refile-folder . "/gmail/All Mail")
+                   (mu4e-trash-folder . "/gmail/Trash")))))
 
-(set-email-account! "kaskshad@showplacewood.com"
-                    '((user-mail-address      . "skaske@showplacewood.com")
-                    (user-full-name         . "Shad Kaske")
-                    (smtpmail-smtp-server   . "smtp.office365.com")
-                    (smtpmail-smtp-service  . 587)
-                    (smtpmail-stream-type   . starttls)
-                    (smtpmail-debug-info    . t)
-                    (mu4e-drafts-folder     . "/Drafts")
-                    (mu4e-refile-folder     . "/Archive")
-                    (mu4e-sent-folder       . "/Sent Items")
-                    (mu4e-trash-folder      . "/Deleted Items")
-                    (mu4e-update-interval   . 300)
-                    ;(mu4e-sent-messages-behavior . 'delete)
-                    )
-                    nil)
+  ;; ;;;; Mailbox Shortcuts
+  ;; (setq mu4e-maildir-shortcuts
+  ;;       '(("/kaskshad-showplace/INBOX" . ?si)
+  ;;         ("/kaskshad-showplace/Drafts" . ?sd)))
+
+  ;;;; Send Mail Settings
+  (setq! sendmail-program "/usr/bin/msmtp"
+         send-mail-function 'smtpmail-send-it
+         message-sendmail-f-is-evil t
+         message-sendmail-extra-arguments '("--read-envelope-from")
+         message-send-mail-function 'message-send-mail-with-sendmail))
 
 (mu4e-alert-set-default-style 'libnotify)
 (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-(setq mu4e-compose-format-flowed t)
 
 ;; Org Mode
 ;;;; Doom Emacs Org Settings
@@ -107,6 +130,9 @@
 
 ;;;; Disable LineNumber in Org Mode
 (add-hook! 'org-mode-hook #'doom-disable-line-numbers-h)
+
+;;;; Set Variable Pitch Fonts for org mode
+(add-hook! 'org-mode-hook 'smk/set-buffer-variable-pitch)
 
 ;;;; Hide Emphasis Markers
 (after! org
@@ -128,22 +154,22 @@
                                  ("HOLD" . "DimGray")))
   ;;;; Org Agenda Files
   (setq org-agenda-files '("~/Nextcloud/org/inbox.org"
-                          "~/Nextcloud/org/projects.org"
-                          "~/Nextcloud/org/tickler.org"
-                          "~/Nextcloud/org/cal/googlecal.org"
-                          "~/Nextcloud/org/cal/showplacecal.org"))
+                           "~/Nextcloud/org/projects.org"
+                           "~/Nextcloud/org/tickler.org"
+                           "~/Nextcloud/org/cal/googlecal.org"
+                           "~/Nextcloud/org/cal/showplacecal.org"))
 
   ;;;; Org Capture Templates
   (setq org-capture-templates
         '(("t" "ToDo [Inbox]"
            entry (file "~/Nextcloud/org/inbox.org")
-                  "* TODO %?\n")
+           "* TODO %?\n")
           ("T" "Tickler"
            entry (file "~/Nextcloud/org/tickler.org")
-                  "* TODO %i%? \nSCHEDULED: <%(org-read-date nil nil \"+1d\")>")
+           "* TODO %i%? \nSCHEDULED: <%(org-read-date nil nil \"+1d\")>")
           ("P" "New Project"
            entry (file "~/Nextcloud/org/projects.org")
-                  "* %i%? ")
+           "* %i%? ")
           ("e" "Email [Inbox]"
            entry (file "~/Nextcloud/org/inbox.org")
            "* TODO %?\n%U\n%a\n")
@@ -181,15 +207,12 @@
   (setq org-ellipsis "  ▼")
   (setq org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●"))
   ;;;; Org Headline Sizes
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 0.9)
-                  (org-level-6 . 0.85)
-                  (org-level-7 . 0.8)
-                  (org-level-8 . 0.75)))))
-
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
+   '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+   '(org-level-4 ((t (:inherit outline-4 :height 0.9))))
+   '(org-level-5 ((t (:inherit outline-5 :height 0.85))))))
 ;;;; Disable Electric Indent Mode
 (add-hook! org-mode (electric-indent-local-mode -1))
 
@@ -223,8 +246,12 @@
           neo-windows-fixed-size nil))
 
 ;; Centaur Tabs
-;; (setq centaur-tabs-set-bar 'over)
-;; (setq centaur-tabs-close-button nil)
+;; (setq centaur-tabs-set-bar 'over
+;;       centaur-tabs-close-button nil)
+
+;;;; Evil Keybindings for Centaur Tabs
+;; (define-key evil-normal-state-map (kbd "g t") 'centaur-tabs-forward)
+;; (define-key evil-normal-state-map (kbd "g T") 'centaur-tabs-backward)
 
 ;; Dired
 
